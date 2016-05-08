70cdf1f21ededa6029a51b58a45e2896b6b4f9af
Andrii Usov <andrii.usov@ericsson.com>
2016-05-07 22:46:44 -0400

    moved source to src

--- AlexaRequestLyft/src/requestLyft.py
+++ AlexaRequestLyft/src/requestLyft.py
@@ -4,6 +4,8 @@ Created on May 7, 2016
 @author: Andrii Usov
 '''
 from __future__ import print_function
+import httplib, urllib2, urllib, json
+from geopy.geocoders import Nominatim
 
 applicationId="amzn1.echo-sdk-ams.app.6dc928c8-e705-4b14-b76d-7ba83e372ce7"
 
@@ -14,26 +16,21 @@ def lambda_handler(event, context):
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
 
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
     if (event['session']['application']['applicationId'] !=
             applicationId):
         raise ValueError("Invalid Application ID")
-
+    
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
                            event['session'])
 
+    
     if event['request']['type'] == "LaunchRequest":
         return on_launch(event['request'], event['session'])
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
         return on_session_ended(event['request'], event['session'])
-
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
@@ -61,12 +58,15 @@ def on_intent(intent_request, session):
 
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
-
+    stage = get_stage_number(session)
+    
     # Dispatch to your skill's intent handlers
     if intent_name == "RequestLyft":
         return request_lyft(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
+    elif intent_name == "AMAZON.YesIntent":
+        return call_a_cab(intent, session)
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
     else:
@@ -92,13 +92,13 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    speech_output = "Welcome to the Alexa Lyft service. " \
+                    "To request a ride from Lyft say, " \
+                    "ask Lyft for a ride"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "Please say, ask Lyft for a ride, " \
+                    "to request a ride from Lyft."
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -106,53 +106,66 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+    speech_output = "Thank you for using Alexa Lyft service. " \
                     "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
-
+# Entry point for ride request
 def request_lyft(intent, session):
     """ gets estimates from Lyft
-    user.
     """
 
     card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
     
-    speech_output = "Getting estimates from Lyft"
+    should_end_session = False
+    
+    speech_output = "There is a Lyft cab 2 minutes away, "  \
+                    "would you like me to order it?"
     reprompt_text = "I will try one more time"
+    
+    session_attributes = {'lyft_cab_id':'123'}
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
+# Entry point for ride request
+def call_a_cab(intent, session):
+    """ gets estimates from Lyft
+    """
 
-    if "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
+    card_title = intent['name']
+    
+    should_end_session = False
+    
+    cab_id = session['attributes']['lyft_cab_id']
+    
+    speech_output = "Ok, calling"
+    reprompt_text = "I will try one more time"
+    
+    session_attributes = {'lyft_cab_id':'123'}
 
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
     return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
+        card_title, speech_output, reprompt_text, should_end_session))
+
+# --------------- Helpers that process requests ----------------------
+
+def get_stage_number(session):
+    if (hasattr(session, 'stage') == False or session['new'] == 'true'):
+        return 0;
+    else:
+        return session['stage']
+
+def get_access_token(session):
+    if hasattr(session['user'], 'accessToken') == True:
+        return session['user']['accessToken']
+    else:
+        raise ValueError("Session should contain access token")
 
 # --------------- Helpers that build all of the responses ----------------------
-
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
@@ -161,7 +174,7 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
             'text': output
         },
         'card': {
-            'type': 'Simple',
+            'type': "Simple",
             'title': 'SessionSpeechlet - ' + title,
             'content': 'SessionSpeechlet - ' + output
         },
@@ -177,7 +190,7 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
 
 def build_response(session_attributes, speechlet_response):
     return {
-        'version': '1.0',
+        'version': '2.0',
         'sessionAttributes': session_attributes,
         'response': speechlet_response
     }
